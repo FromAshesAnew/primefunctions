177dcf91694a1194eecb795275b958d1868080d5
AndrewBeckers <glenn.beckers@wsu.edu>
2019-10-24 16:50:33 -0700

    Finished primeGen.

---
+++ primefunctions.js
@@ -0,0 +1,22 @@
+function primeCheck(threshold) {
+  let i;
+  for (i = 2; i < threshold; i++) {
+    if (threshold % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function primeGen(threshold) {
+  const list = [];
+  let i;
+  for (i = 2; i < threshold; i++) {
+    if (primeCheck(i) === true) {
+      list.push(i);
+    }
+  }
+  return list;
+}
+console.log('PrimeGen Test Results');
+console.log(primeGen(10));
