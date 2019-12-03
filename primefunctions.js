901a13a2b83c2f075a8bf270748d09e6cad7a95a
AndrewBeckers <glenn.beckers@wsu.edu>
2019-12-03 01:35:57 -0800

    maxPrimeSum() working correctly

--- primefunctions.js
+++ primefunctions.js
@@ -34,39 +34,38 @@ function cumulativeSum(list) {
 console.log('Cumulative Sum Test Results');
 console.log(cumulativeSum([1, 2, 3, 4]));
 
+  function maxPrimeSum(threshold) {
+    const Primes = primeGen(threshold);
+    let biggestSums = 0;
+    let Prime = 0;
 
-function maxPrimeSum(threshold) {
-  const list = [];
-  let i;
-  let count = 0;
-  let total = 0;
-  const primes = primeGen(threshold);
+    for (let i = 0; Primes[i] < threshold; i++) {
+      for (let j = 0; j < i; j++) {
+        let list = [];
+        for (let k = j; k < i; k++) {
+          list.push(Primes[k]);
+        }
+        list = cumulativeSum(list);
+        const sums = list[list.length - 1];
 
-  while (total <= threshold) {
-    for (i = 0; i < primes.length; i++) {
-      count++;
-      total += primes[i];
-      if (total > threshold) {
-        break;
+        if (sums > threshold) {
+          break;
+        }
+
+        if (Primes.includes(sums)) {
+          if (biggestSums <= i - j) {
+            biggestSums = i - j;
+            Prime = sums;
+          }
+        }
       }
     }
+    return [Prime, biggestSums];
   }
-  if (primeCheck(total) === true) {
-    list.push(total);
-  } else {
-    while (primeCheck(total) === false) {
-      total -= primes[i];
-      i--;
-      count--;
-    }
-    list.push(total);
-    list.push(count);
-  }
-  return list;
-}
 
 const highestPrime = maxPrimeSum(100);
 console.log('MaxPrimeSum Test Results');
 console.log(highestPrime);
+
 
 
--- testCorrectness.test.js
+++ testCorrectness.test.js
@@ -3,9 +3,11 @@
 
 describe('primefunctions', function () {
   const panswer = [2, 3, 5, 7];
-  const panswer2 = [2, 3, 5, 7, 11, 13, 17, 19]
-  const canswer = [1, 3, 6, 10]
-  const canswer2 = [10, 21, 33, 46, 60]
+  const panswer2 = [2, 3, 5, 7, 11, 13, 17, 19];
+  const canswer = [1, 3, 6, 10];
+  const canswer2 = [10, 21, 33, 46, 60];
+  const manswer = [41, 6];
+  const manswer2 = [953, 21];
 
   describe('primeGen()', function () {
     it('primeGen(10) = [2, 3, 5, 7]', function () {
@@ -22,6 +24,14 @@ describe('primefunctions', function () {
     });
     it('cumlativeSum([10, 11, 12, 13, 14]) = [10, 21, 33, 46, 60]', function () {
       chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.eql(canswer2);
+    });
+  });
+  describe('maxPrimeSum()', function () {
+    it('maxPrimeSum(100) = [41, 6] ', function () {
+      chai.expect(maxPrimeSum(100)).to.eql(manswer);
+    });
+    it('maxPrimeSum(1000) = [953, 21]', function () {
+      chai.expect(maxPrimeSum(1000)).to.eql(manswer2);
     });
   });
 });
