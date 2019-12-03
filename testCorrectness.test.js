5a597856fca6e138659cf6c5759d6ac96c137f97
AndrewBeckers <glenn.beckers@wsu.edu>
2019-12-02 23:28:13 -0800

    cumlativeSum() working correctly

--- testCorrectness.test.js
+++ testCorrectness.test.js
@@ -15,4 +15,13 @@ describe('primefunctions', function () {
       chai.expect(primeGen(20)).to.eql(panswer2);
     });
   });
+
+  describe('cumulativeSum()', function () {
+    it('cumulativeSum([1, 2, 3, 4]) = [1, 3, 6, 10] ', function () {
+      chai.expect(cumulativeSum([1, 2, 3 ,4])).to.eql(canswer);
+    });
+    it('cumlativeSum([10, 11, 12, 13, 14]) = [10, 21, 33, 46, 60]', function () {
+      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.eql(canswer2);
+    });
+  });
 });
