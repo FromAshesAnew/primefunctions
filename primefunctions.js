e058a7b41e770b6ca6f597ce0efd0ef3244dd8e8
AndrewBeckers <glenn.beckers@wsu.edu>
2019-12-03 16:41:00 -0800

    maxPrimeSum fix

--- primefunctions.js
+++ primefunctions.js
@@ -38,29 +38,32 @@ console.log(cumulativeSum([1, 2, 3, 4]));
     const Primes = primeGen(threshold);
     let biggestSums = 0;
     let Prime = 0;
+    let list2 = [];
 
-    for (let i = 0; Primes[i] < threshold; i++) {
-      for (let j = 0; j < i; j++) {
-        let list = [];
-        for (let k = j; k < i; k++) {
-          list.push(Primes[k]);
+    for (let outer = 0; Primes[outer] < threshold; outer++) {
+      for (let inner = 0; inner < outer; inner++) {
+        let primeList = [];
+        for (let middle = inner; middle < outer; middle++) {
+          primeList.push(Primes[middle]);
         }
-        list = cumulativeSum(list);
-        const sums = list[list.length - 1];
+        primeList = cumulativeSum(primeList);
+        const sums = primeList[primeList.length - 1];
 
         if (sums > threshold) {
           break;
         }
 
-        if (Primes.includes(sums)) {
-          if (biggestSums <= i - j) {
-            biggestSums = i - j;
+        if (primeCheck(sums) == true) {
+          if (biggestSums <= outer - inner) {
+            biggestSums = outer - inner;
             Prime = sums;
           }
         }
       }
     }
-    return [Prime, biggestSums];
+    list2.push(Prime);
+    list2.push(biggestSums);
+    return list2;
   }
 
 const highestPrime = maxPrimeSum(100);
