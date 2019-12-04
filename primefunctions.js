450745bbf41071a79e6cc7d42b06b3c40ebb7745
AndrewBeckers <glenn.beckers@wsu.edu>
2019-12-03 16:44:35 -0800

    maxPrime fix

--- primefunctions.js
+++ primefunctions.js
@@ -34,38 +34,38 @@ function cumulativeSum(list) {
 console.log('Cumulative Sum Test Results');
 console.log(cumulativeSum([1, 2, 3, 4]));
 
-  function maxPrimeSum(threshold) {
-    const Primes = primeGen(threshold);
-    let biggestSums = 0;
-    let Prime = 0;
+function maxPrimeSum(threshold) {
+  const Primes = primeGen(threshold);
+  let biggestSums = 0;
+  let Prime = 0;
+  let list2 = [];
 
-    for (let i = 0; Primes[i] < threshold; i++) {
-      for (let j = 0; j < i; j++) {
-        let list = [];
-        for (let k = j; k < i; k++) {
-          list.push(Primes[k]);
-        }
-        list = cumulativeSum(list);
-        const sums = list[list.length - 1];
+  for (let outer = 0; Primes[outer] < threshold; outer++) {
+    for (let inner = 0; inner < outer; inner++) {
+      let primeList = [];
+      for (let middle = inner; middle < outer; middle++) {
+        primeList.push(Primes[middle]);
+      }
+      primeList = cumulativeSum(primeList);
+      const sums = primeList[primeList.length - 1];
 
-        if (sums > threshold) {
-          break;
-        }
+      if (sums > threshold) {
+        break;
+      }
 
-        if (Primes.includes(sums)) {
-          if (biggestSums <= i - j) {
-            biggestSums = i - j;
-            Prime = sums;
-          }
+      if (primeCheck(sums) == true) {
+        if (biggestSums <= outer - inner) {
+          biggestSums = outer - inner;
+          Prime = sums;
         }
       }
     }
-    return [Prime, biggestSums];
   }
+  list2.push(Prime);
+  list2.push(biggestSums);
+  return list2;
+}
 
 const highestPrime = maxPrimeSum(100);
 console.log('MaxPrimeSum Test Results');
 console.log(highestPrime);
-
-
-
